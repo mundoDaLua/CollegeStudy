edda87b24c6e33a4920fff402341a1ff1a1614d9
barcelosluan <luanbarcelos.tech@gmail.com>
2020-02-14 20:46:23 -0300

    Aula do dia 14-02-2020

---
+++ estruturaDeDados/src/aula05/Livro.java
@@ -0,0 +1,45 @@
+package aula05;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private String editora;
+
+    public Livro() {
+
+    }
+
+    public Livro(String autor, String editora) {
+        this.autor = autor;
+        this.editora = editora;
+        this.titulo = titulo;
+    }
+
+    public Livro(String titulo, String autor, String editora) {
+        this.autor = autor;
+        this.editora = editora;
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+}
---
+++ estruturaDeDados/src/aula05/Main.java
@@ -0,0 +1,28 @@
+package aula05;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Livro livro = new Livro();
+
+        System.out.println("Informe o titulo do livro: ");
+        livro.setTitulo(sc.nextLine());
+        System.out.println("Informe o autor do livro " + livro.getTitulo());
+        livro.setAutor(sc.nextLine());
+        System.out.println("Informe a editora do livro " + livro.getTitulo());
+        livro.setEditora(sc.nextLine());
+
+        System.out.println("Livro " + livro.getTitulo() + " do autor " + livro.getAutor() +
+                " da editora " + livro.getEditora() + " adicionado com sucesso!");
+
+
+
+
+    }
+
+}
