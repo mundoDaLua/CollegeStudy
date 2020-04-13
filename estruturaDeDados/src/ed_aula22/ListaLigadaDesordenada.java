cccd2e0481233d2cca22ac74080d1b2c336fea8e
barcelosluan <luanbarcelos.tech@gmail.com>
2020-04-13 18:03:38 -0300

    ed_aula22 Implementação de uma Lista Ligada Desordenada

--- estruturaDeDados/src/ed_aula22/ListaLigadaDesordenada.java
+++ estruturaDeDados/src/ed_aula22/ListaLigadaDesordenada.java
@@ -15,20 +15,33 @@ public class ListaLigadaDesordenada {
         return fruta == null;
     }
 
+    // Busca uma fruta na ListaLigadaDesordenada
+    public Fruta search(Fruta fruta) {
+        if (isEmpity()) {
+            return null;
+        }
+
+        Node i;
+        for (i = this.fruta; i != null && !fruta.getNome().equals(i.getAtual().getNome());i = i.getProximo());
+        if (i == null) {
+            return null;
+        }
+        return i.getAtual();
+    }
+
     /*
      * Procura se a Fruta jaTinha na ListaLigadaDesordenada
      * Se encontrar, retorna um Erro
      * Caso contrario, constroi um Node novo da novaFruta
      * Se a ListaLigadaDesordenada estiver vazia, atribui o Node novo ao inicio
-     * Caso contrario, Atribui o Node novo ao Node proximo do finalFila, para ele sempre referenciar o Node que
-     * está atraz dele
-     * Atribui o Node adicionado ao finalFila
-     * Aumenta em uma unidade o tamanhoFila
+     * Caso contrario, Atribui o Node novo ao Node proximo do novo, para ele sempre referenciar o Node que vinha antes
+     * Atribui o Node novo ao fruta da ListaLigadaDesordenada
+     * Aumenta em uma unidade o tamanho
      */
-    public void add(Fruta novaFruta) throws Exception {
+    public void add(Fruta novaFruta) {
         Fruta jaTinha = search(novaFruta);
         if (jaTinha != null) {
-            throw new Exception("    Erro: Fruta não adicionada, pois ja constava na Lista!");
+            System.out.println("    Erro: Fruta não adicionada, pois ja constava na Lista!");
         }
 
         Node novo = new Node(novaFruta);
@@ -41,6 +54,58 @@ public class ListaLigadaDesordenada {
         tamanho++;
     }
 
+    /*
+     * Verifica se a ListaLigadaDesordenada está vazia
+     * Se estiver, retorna um erro
+     * Caso contrario, cria Nodes auxiliares (i == frutaAtual, j == frutaAnterior) e Fruta auxiliar (frutaDeletada) para encontrar a Fruta fruta
+     * Se a busca nao encontrar (i == null) retorna null
+     * Se a busca encontrar (fruta.nome == i.atual.nome) a frutaDeletada recebe i.atual
+     *
+     * Na terceira parte do metodo delete, faz o tratamento para que a ListaLigadaDesordenada nao tenha "buracos"
+     * pois caso tenham espaços vazios, as Frutas deixam de estar ligadas, parando de funcionar a classe
+     * Se os Node auxiliares forem iguais, a this.fruta vai passar a referenciar a this.fruta.proximo
+     * Caso contrario, a Fruta anterior passa a apontar para a frutaAtual
+     * Reduz o tamanho da ListaLigadaDesordenada em uma unidade
+     * Retorna a frutaDeletada
+     */
+    public Fruta delete(Fruta fruta) {
+        if (isEmpity()) {
+            return null;
+        }
 
+        Node i;
+        Node j;
+        Fruta frutaDeletada;
+        for (i = this.fruta, j = this.fruta; i != null && !fruta.getNome().equals(i.getAtual().getNome());
+             j = i, i = i.getProximo());
+        if (i == null) {
+            return null;
+        }
+        frutaDeletada = i.getAtual();
+
+        if (i == j) {
+            this.fruta = this.fruta.getProximo();
+        } else {
+            j.setProximo(i.getProximo());
+        }
+        tamanho--;
+        return frutaDeletada;
+    }
+
+    // Informa todas as Frutas presentes na ListaLigadaDesordenada
+    public String print() {
+        if (isEmpity()) {
+            return "Lista Vazia";
+        }
+
+        String listaFruta = "[";
+        Node i;
+        int j;
+        for (i = fruta,j = 1; i != null; i = i.getProximo(), j++) {
+            listaFruta += " Fruta " + j + ": " + i.getAtual().getNome() + ";";
+        }
+
+        return listaFruta + " ]";
+    }
 
 }
--- estruturaDeDados/src/ed_aula22/Main.java
+++ estruturaDeDados/src/ed_aula22/Main.java
@@ -1,4 +1,27 @@
 package ed_aula22;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        ListaLigadaDesordenada listaFruta = new ListaLigadaDesordenada();
+
+        System.out.println("Vazio? " + listaFruta.isEmpity());
+        System.out.println("Tamanho da Lista: " + listaFruta.getTamanho());
+
+        Fruta maca = new Fruta("Maçã");
+        Fruta banana = new Fruta("Banana");
+        listaFruta.add(maca);
+        listaFruta.add(banana);
+        System.out.println("Vazio? " + listaFruta.isEmpity());
+        System.out.println("Tamanho da Lista: " + listaFruta.getTamanho());
+        System.out.println("Removido da Lista: " + listaFruta.delete(maca));
+        System.out.println("Vazio? " + listaFruta.isEmpity());
+        System.out.println("Tamanho da Lista: " + listaFruta.getTamanho());
+        System.out.println("Removido da Lista: " + listaFruta.delete(banana));
+        System.out.println("Vazio? " + listaFruta.isEmpity());
+        System.out.println("Tamanho da Lista: " + listaFruta.getTamanho());
+
+    }
+
 }
